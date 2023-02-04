ad9e9abfda77b1d371fc2146c249e9f3e07d91a8
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-02-04 17:12:44 +0200

    Finished task 2. Was realized work with console in ConsoleHelper class. Added task 3.

--- src/main/ConsoleHelper.java
+++ src/main/ConsoleHelper.java
@@ -1,4 +1,35 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage (String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String line = "";
+        try {
+             line = bufferedReader.readLine();
+        } catch (Exception e) {
+            writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            readString();
+        }
+        return line;
+    }
+
+    public static int readInt() {
+        int line = 0;
+        try {
+            line = Integer.parseInt(readString());
+        } catch (NumberFormatException e) {
+            writeMessage("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            readInt();
+        }
+        return line;
+    }
 }
