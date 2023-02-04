0fe37f0ef370ae2ee83268316c0dd6a434417541
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-02-04 19:18:36 +0200

    Finished task 4. Added constructor into Message class. Message class is serializable. Added task 5.

--- src/main/Message.java
+++ src/main/Message.java
@@ -1,4 +1,26 @@
 package main;
 
-public class Message {
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    final MessageType type;
+    final String data;
+
+    public Message(MessageType type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public Message(MessageType type) {
+        this.type = type;
+        data = null;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public String getData() {
+        return data;
+    }
 }
