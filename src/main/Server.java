d871dc2d2289716617523a684c828932f070ddfd
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-02-04 21:09:16 +0200

    Finished task 6. Realized server connection with Sockets and ServerSockets. Added task 7.

--- src/main/Server.java
+++ src/main/Server.java
@@ -1,8 +1,44 @@
 package main;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int port = ConsoleHelper.readInt();
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (Exception e) {
+
+        }
+        ConsoleHelper.writeMessage("Server is running");
+        Socket clientSocket;
+        Handler handler;
+        while (true) {
+            clientSocket = null;
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (Exception e) {
+                serverSocket.close();
+                System.err.println(e.getMessage());
+                break;
+            }
+            if (clientSocket != null) {
+                handler = new Handler(clientSocket);
+                handler.run();
+            }
+
+        }
+    }
+
+    private static class Handler extends Thread {
+        Socket socket;
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
 
     }
 }
