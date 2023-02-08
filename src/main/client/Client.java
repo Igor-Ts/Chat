270bf659289dabd6c3298773d98e22763a576935
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-02-08 22:03:09 +0200

    Finished task 13. Added utility methods into Client class. Added task 14.

--- src/main/client/Client.java
+++ src/main/client/Client.java
@@ -1,10 +1,47 @@
 package main.client;
 
 import main.Connection;
+import main.ConsoleHelper;
+import main.Message;
+import main.MessageType;
+
+import java.io.IOException;
 
 public class Client {
-    private Connection connection;
+    protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("Please, write server address (localhost, ip)");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("Please, write server port");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        ConsoleHelper.writeMessage("Please, write username");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() { // need to override if u want to close this opportunity
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT,text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Connection lost");
+            clientConnected = false;
+        }
+    }
 
     public static class SocketThread extends Thread {
 
