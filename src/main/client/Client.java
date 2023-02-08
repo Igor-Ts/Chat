42261dfc16af3af9abff647684c7eea064ac3fc7
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-02-08 21:39:02 +0200

    Finished task 12. Strated client realization. Added new package and main class Client. Added task 13.

---
+++ src/main/client/Client.java
@@ -0,0 +1,12 @@
+package main.client;
+
+import main.Connection;
+
+public class Client {
+    private Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public static class SocketThread extends Thread {
+
+    }
+}
